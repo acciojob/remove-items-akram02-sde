f1cf9bff038eabe39f4739321131d613c92447f3
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-02 16:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let btn = document.getElementsByTagName('input')[0];
+let select = document.getElementById('colorSelect')
+
+btn.addEventListener('click', () => {
+	select.options[select.selectedIndex].remove();
+})
